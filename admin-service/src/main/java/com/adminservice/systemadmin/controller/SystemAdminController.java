8505754dc3d793cda4e065f63259bae56d9b8505
Prad001 <shelkepradeep76@gmail.com>
2025-05-21 13:25:21 +0530

    add post and get method for system-admin 2.0

--- admin-service/src/main/java/com/adminservice/systemadmin/controller/SystemAdminController.java
+++ admin-service/src/main/java/com/adminservice/systemadmin/controller/SystemAdminController.java
@@ -10,13 +10,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.adminservice.systemadmin.dto.SystemAdminRequestDTO;
 import com.adminservice.systemadmin.dto.SystemAdminResponseDTO;
@@ -24,27 +22,18 @@ import com.adminservice.systemadmin.service.SystemAdminService;
 
 import jakarta.validation.groups.Default;
 
-
 @RestController
 @RequestMapping("/system-admins")
 public class SystemAdminController {
-      private final SystemAdminService systemAdminService;
+    private final SystemAdminService systemAdminService;
 
     public SystemAdminController(SystemAdminService systemAdminService) {
         this.systemAdminService = systemAdminService;
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<SystemAdminResponseDTO> updateSystemAdmin(@PathVariable("id") UUID systemAdminId,
-            @Validated({ Default.class }) @RequestBody SystemAdminRequestDTO systemAdminRequestDTO) {
-        SystemAdminResponseDTO systemAdminResponseDTO = systemAdminService
-                .updateSystemAdmin(systemAdminId, systemAdminRequestDTO);
-        return ResponseEntity.ok().body(systemAdminResponseDTO);
-
-            }
-
     @PostMapping
-    public ResponseEntity<SystemAdminResponseDTO> createSystemAdmin(@RequestBody SystemAdminRequestDTO systemAdminRequestDTO) {
+    public ResponseEntity<SystemAdminResponseDTO> createSystemAdmin(
+            @RequestBody SystemAdminRequestDTO systemAdminRequestDTO) {
         SystemAdminResponseDTO systemAdminResponseDTO = systemAdminService.createSystemAdmin(systemAdminRequestDTO);
         return ResponseEntity.ok().body(systemAdminResponseDTO);
     }
@@ -54,7 +43,16 @@ public class SystemAdminController {
         List<SystemAdminResponseDTO> systemAdmins = systemAdminService.getSystemAdmins();
         return ResponseEntity.ok().body(systemAdmins);
     }
-    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SystemAdminResponseDTO> updateSystemAdmin(@PathVariable("id") UUID systemAdminId,
+            @Validated({ Default.class }) @RequestBody SystemAdminRequestDTO systemAdminRequestDTO) {
+        SystemAdminResponseDTO systemAdminResponseDTO = systemAdminService
+                .updateSystemAdmin(systemAdminId, systemAdminRequestDTO);
+        return ResponseEntity.ok().body(systemAdminResponseDTO);
+
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteSystemAdminById(@PathVariable("id") UUID systemAdminId) {
         systemAdminService.deleteSystemAdminById(systemAdminId);
--- admin-service/src/main/java/com/adminservice/systemadmin/service/SystemAdminService.java
+++ admin-service/src/main/java/com/adminservice/systemadmin/service/SystemAdminService.java
@@ -13,39 +13,36 @@ import com.adminservice.systemadmin.repository.SystemAdminRepository;
 
 import jakarta.transaction.Transactional;
 
-
 import java.util.List;
 
-import com.adminservice.systemadmin.validator.SystemAdminValidator;
 
 @Service
 public class SystemAdminService {
-      private SystemAdminRepository systemAdminRepository;
-    private SystemAdminValidator systemAdminValidator;
+    private SystemAdminRepository systemAdminRepository;
 
-    public SystemAdminService(SystemAdminRepository systemAdminRepository, SystemAdminValidator systemAdminValidator) {
+    public SystemAdminService(SystemAdminRepository systemAdminRepository) {
         this.systemAdminRepository = systemAdminRepository;
-        this.systemAdminValidator = systemAdminValidator;
+
     }
 
     public SystemAdminResponseDTO createSystemAdmin(SystemAdminRequestDTO systemAdminRequestDTO) {
-        systemAdminValidator.validateForCreation(systemAdminRequestDTO);
 
         SystemAdmin newSystemAdmin = systemAdminRepository.save(SystemAdminMapper.toModel(systemAdminRequestDTO));
 
         return SystemAdminMapper.toDto(newSystemAdmin);
     }
 
-     public List<SystemAdminResponseDTO> getSystemAdmins() {
+    public List<SystemAdminResponseDTO> getSystemAdmins() {
         List<SystemAdmin> systemAdmins = systemAdminRepository.findAll();
 
-        List<SystemAdminResponseDTO> systemAdminResponseDTOs = systemAdmins.stream().map(systemAdmin -> SystemAdminMapper.toDto(systemAdmin))
+        List<SystemAdminResponseDTO> systemAdminResponseDTOs = systemAdmins.stream()
+                .map(systemAdmin -> SystemAdminMapper.toDto(systemAdmin))
                 .toList();
 
         return systemAdminResponseDTOs;
     }
 
-     public SystemAdminResponseDTO updateSystemAdmin(UUID systemAdminId, SystemAdminRequestDTO systemAdminRequestDTO) {
+    public SystemAdminResponseDTO updateSystemAdmin(UUID systemAdminId, SystemAdminRequestDTO systemAdminRequestDTO) {
         SystemAdmin systemAdmin = systemAdminRepository.findById(systemAdminId)
                 .orElseThrow(() -> new SystemAdminNotFoundException(
                         "System admin not found with ID: " + systemAdminRequestDTO.getSystemAdminId()));
@@ -58,7 +55,7 @@ public class SystemAdminService {
 
         return SystemAdminMapper.toDto(updatedSystemAdmin);
     }
-    
+
     @Transactional
     public void deleteSystemAdminById(UUID systemAdminId) {
         SystemAdmin systemAdmin = systemAdminRepository.findById(systemAdminId)
@@ -66,6 +63,5 @@ public class SystemAdminService {
                         "System Admin not found with ID: " + systemAdminId));
         systemAdminRepository.delete(systemAdmin);
     }
-
 
 }
