f401daec18cf7a55481b7d14541909b4595dd2d9
Atharva Jadhav <drdrunkenstien10@gmail.com>
2025-07-26 15:19:30 +0530

    Add web config for appointment service

---
+++ appointment-service/src/main/java/com/appointmentservice/appointment/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.appointmentservice.appointment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @SuppressWarnings("null")
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/appointments/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // Optional: If you're using cookies/auth
+            }
+        };
+    }
+}
